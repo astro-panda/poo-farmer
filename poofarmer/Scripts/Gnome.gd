8eed14bad81b36a4673e743d5588e77eed1752dd
Turner Bass <turner@astropanda.studio>
2023-01-06 22:32:06 -0600

    beginning gnome player detection

---
+++ poofarmer/Scripts/Gnome.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var player_in_range = false
+
+onready var sprite = $AnimatedSprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_Area2D_body_entered(body: Node):
+	if(body.is_in_group("player")):
+		sprite.animation = "glow"
+		player_in_range = true
+
+
+func _on_Area2D_body_exited(body: Node):	
+	if(body.is_in_group("player")):
+		sprite.animation = "default"
+		player_in_range = false
+
+
+func _unhandled_input(event):
+	var should_open_store = event is InputEventMouseButton && (event as InputEventMouseButton).button_index == 1 && player_in_range
+	if(should_open_store):
+		print("clicky clicky");
