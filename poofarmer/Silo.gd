1c8ee28b8995733a0d3764faa6d50e44a558d333
Semnor <leithprise@gmail.com>
2023-01-06 19:02:57 -0600

    add silo node and colliders

---
+++ poofarmer/Silo.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+signal hit
+
+export var poo_total = 0
+
+func _ready():
+	pass # Replace with function body.
+
+
+func player_enters(body):
+	print("Player entered silo")
+	# TODO: Add player poo carried to poo total
+	pass
