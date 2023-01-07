c04de874656419bfa579774d8bc1bf67ae1018e2
Ethan Hair <ethan@astropanda.studio>
2023-01-06 20:50:26 -0600

    Added the hold amount and capacity

--- poofarmer/Scripts/Player.gd
+++ poofarmer/Scripts/Player.gd
@@ -1,16 +1,60 @@
-extends RigidBody2D
+extends Area2D
 
+
+signal playerFire(playerPosition, fireAngle)
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export var speed = 300
+export var holdCapacity = 10
+var currentHoldAmount = 0
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$PlayerSprite.play()
+	else:
+		$PlayerSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+	if velocity.x > 0:
+		print("play right animation")
+	elif velocity.x < 0:
+		print("play left animation")
+	elif velocity.y > 0:
+		print("play up animation")
+	elif velocity.y < 0:
+		print ("play down animation")
+		
+func _unhandled_input(event):
+	if event.is_action_pressed("player_fire"):
+		var clickLocation = get_global_mouse_position()
+		var fireAngle = rad2deg(clickLocation.angle_to_point(position))
+		emit_signal("playerFire", position, fireAngle)
+		print("fire from ", position, " at an angle of ", fireAngle)
+
+
+func _on_Player_body_entered(body):
+	# TODO: Have a check that the body object is in a poo group
+	if (currentHoldAmount < holdCapacity):
+		currentHoldAmount += 1
+		print("grabbed a poo")
