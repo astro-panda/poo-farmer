8d7911ff02638f55b8edc9b50c3fc82089bd724f
Turner Bass <turner@astropanda.studio>
2023-01-07 13:12:04 -0600

    Transitions from Main music to store and pause music after player interaction

--- poofarmer/Scripts/Gnome.gd
+++ poofarmer/Scripts/Gnome.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal store_opened(opened)
+
 var player_in_range = false
 
 onready var sprite = $AnimatedSprite
@@ -7,6 +9,13 @@ onready var sprite = $AnimatedSprite
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func _unhandled_input(event):	
+	var should_open_store = event is InputEventMouseButton && (event as InputEventMouseButton).button_index == 1 && player_in_range && event.is_pressed()
+	if should_open_store:
+		emit_signal("store_opened", true)
+	elif event.is_action_pressed("store"):
+		emit_signal("store_opened", false)
 
 func _on_Gnome_area_entered(body: Node):
 	if(body.is_in_group("player")):
@@ -18,9 +27,4 @@ func _on_Gnome_area_exited(body: Node):
 	if(body.is_in_group("player")):
 		sprite.animation = "default"
 		player_in_range = false
-
-
-func _unhandled_input(event):
-	var should_open_store = event is InputEventMouseButton && (event as InputEventMouseButton).button_index == 1 && player_in_range && event.is_pressed()
-	if(should_open_store):
-		print("clicky clicky");
+	
--- poofarmer/Scripts/ModalWindow.gd
+++ poofarmer/Scripts/ModalWindow.gd
@@ -1,15 +1,27 @@
 extends Control
 
+signal game_play_state_changed(playing)
+
 var is_paused = false setget set_is_paused
 
+func _ready():
+	visible = false
+	
+
 func _unhandled_input(event):
-	if event.is_action_pressed("pause"):
+	if event.is_action_pressed("pause") || event.is_action_pressed("store"):
 		self.is_paused = !is_paused
 
 func set_is_paused(value):
 	is_paused = value
 	get_tree().paused = is_paused
 	visible = is_paused
+	emit_signal("game_play_state_changed", !is_paused)
 	
 func _on_StartButton_pressed():
 	self.is_paused = false
+	emit_signal("game_play_state_changed", !is_paused)
+
+
+func _on_Gnome_store_opened(opened):
+	set_is_paused(opened)
