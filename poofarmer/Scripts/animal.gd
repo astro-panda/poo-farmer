2bcaca4382b2fa24ba06c57aa266ff24a442fb06
Turner Bass <turner@astropanda.studio>
2023-01-06 18:49:10 -0600

    finished poo loop

--- poofarmer/Scripts/animal.gd
+++ poofarmer/Scripts/animal.gd
@@ -1,15 +1,13 @@
 extends RigidBody2D
 
-export(float) var poo_timer_min = 4
-export(float) var poo_timer_max = 20
+export(float) var poo_timer_min = 4.0
+export(float) var poo_timer_max = 20.0
 
 onready var poo_timer = $PooTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():	
 	reset_poo_timer()
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -17,8 +15,10 @@ func _ready():
 
 
 func reset_poo_timer():
-	poo_timer.wait = rand_range(poo_timer_min, poo_timer_max)
-	poo_timer.start()
+	poo_timer.wait_time = rand_range(poo_timer_min, poo_timer_max) as float
+	poo_timer.start()	
 	
-func do_poo():
-	pass
+func do_poo():	
+	poo_timer.stop()
+	print("Animal done pooed!💩💩💩💩");
+	reset_poo_timer()
