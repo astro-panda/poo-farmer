556068216b3359f79d6ca1c2a3ab07aadc8d674c
Semnor <leithprise@gmail.com>
2023-01-06 23:34:28 -0600

    add modal scene

---
+++ poofarmer/Scripts/ModalWindow.gd
@@ -0,0 +1,15 @@
+extends Control
+
+var is_paused = false setget set_is_paused
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		self.is_paused = !is_paused
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
+	
+func _on_StartButton_pressed():
+	self.is_paused = false
