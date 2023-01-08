ef61814dd458c013a358e04355946b99c39a8638
Turner Bass <turner@astropanda.studio>
2023-01-07 23:15:53 -0600

    Player global poo meter label update

--- poofarmer/Scripts/Silo.gd
+++ poofarmer/Scripts/Silo.gd
@@ -1,14 +1,1 @@
 extends Area2D
-
-signal hit
-
-export var poo_total = 0
-
-func _ready():
-	pass # Replace with function body.
-
-
-func player_enters(body):
-	print("Player entered silo")
-	# TODO: Clear player hold capacity
-	pass
