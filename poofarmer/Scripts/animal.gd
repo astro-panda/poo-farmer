e41ebdbe7ac2835a144dc3d61df887523ae490e7
Turner Bass <turner@astropanda.studio>
2023-01-07 20:47:22 -0600

    Finished Mob Audio controller wire up for behaviors and actions

--- poofarmer/Scripts/animal.gd
+++ poofarmer/Scripts/animal.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+signal action_enacted(animalType)
+
 export(float) var poo_timer_min = 4.0
 export(float) var poo_timer_max = 20.0
 export var speed = 1500
@@ -10,26 +12,10 @@ onready var poo_timer = $PooTimer
 onready var player = get_tree().get_nodes_in_group("player")[0]
 onready var pooSpawner = get_tree().get_nodes_in_group("poo_spawner")[0]
 
-onready var chicken_audio_ctrl = $ChickenAudioController
-onready var cow_audio_ctrl = $CowAudioController
-onready var goat_audio_ctrl = $GoatAudioController 
-onready var llama_audio_ctrl = $LlamaAudioController
-onready var unicorn_audio_ctrl = $UnicornAudioController
-
 var destination: Vector2
 
-var current_audio_controller: MobAudioController
-
-var audio_controllers: Dictionary = {
-	AnimalType.values.Chicken: chicken_audio_ctrl,
-	AnimalType.values.Goat: goat_audio_ctrl,
-	AnimalType.values.Cow: cow_audio_ctrl,
-	AnimalType.values.Llama: llama_audio_ctrl,
-	AnimalType.values.Unicorn: unicorn_audio_ctrl,
-}
-
 # Called when the node enters the scene tree for the first time.
-func _ready():	
+func _ready():
 	destination = Vector2(randi() % 1024, randi() % 1024)
 	reset_poo_timer()
 
@@ -48,16 +34,13 @@ func reset_poo_timer():
 
 
 func do_poo():	
-	pooSpawner.do_poo(self)
-	if (current_audio_controller):
-		var action_index = randi() % current_audio_controller.actions.size()	
-		current_audio_controller.act(action_index)
+	pooSpawner.do_poo(self)	
+	emit_signal("action_enacted", _type)
 	reset_poo_timer()
-
 
 func set_type(type:AnimalType):
 	_type = type.current_value
 	print("Animal Type Selected: " + AnimalType.values.keys()[_type])
 	$Sprite.animation = AnimalType.values.keys()[_type] + " - down"
 	print(_type)
-	current_audio_controller = audio_controllers[_type]
+	
