4a9d5bd689fd15f2dbc786eda3917e5a2c7dc058
Caleb Haldane <caldane@gmail.com>
2023-01-09 13:26:29 -0600

    Fix enemy wave generation

--- poofarmer/Scripts/EnemySpawner.gd
+++ poofarmer/Scripts/EnemySpawner.gd
@@ -2,22 +2,30 @@ extends Node
 
 onready var player = get_tree().get_nodes_in_group("player")[0]
 onready var goblins = $Goblins
+onready var Timers = {
+	"spawn": $SpawnTimer, 
+	"generation": $GenerationTimer, 
+	"hud": $HudUpdateTimer
+}
 
 export(PackedScene) var enemy_scene
 export var maxEnemies = 20
-export var maxSpawnTime = 30.0
-export var minSpawnTime = 10.0
+export var maxSpawnTime = 130.0
+export var minSpawnTime = 80.0
 var rnd = RandomNumberGenerator.new()
 var goblin_population = 20
 var wave_count = 1
 var current_population
+var population_countdown
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	current_population = goblin_population + (randi() % 20) - 10
-	$SpawnTimer.start()
-	$HudUpdateTimer.start()
-	
+	current_population = 3
+	population_countdown = current_population
+	Timers.spawn.start()
+	Timers.hud.start()
+	Timers.generation.stop()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -43,20 +51,25 @@ func _on_SpawnTimer_timeout():
 		enemy.fleeingVector = spawnLoc
 		enemy.connect("global_poo_stolen", player, "_on_Goblin_global_poo_stolen")
 		
-		current_population -= 1
-		goblins.add_child(enemy)
-		if current_population == 0:
-			wave_count += 1
-			$SpawnTimer.stop()
-			$GenerationTimer.start()
+		if population_countdown > 0:
+			population_countdown -= 1
+			goblins.add_child(enemy)
+		else:
+			if goblins.get_child_count() == 0 && Timers.generation.is_stopped():
+				Timers.generation.wait_time = rand_range(minSpawnTime, maxSpawnTime)
+				Timers.generation.start()
 		
-	$SpawnTimer.wait_time = rand_range(minSpawnTime, maxSpawnTime)
 
 
 func _on_GenerationTimer_timeout():
+	wave_count += 1
 	var base_count = goblin_population * wave_count
-	current_population = base_count + (randi() % (base_count)) - base_count
-	$SpawnTimer.start()
+	current_population = base_count + (randi() % (base_count/2)) - (base_count/2)
+	population_countdown = current_population
+	Timers.spawn.start()
 	
 func _on_HudUpdateTimer_timeout():
-	goblins.update_goblin_hud()
+	var waveInfo = wave_count
+	if !Timers.generation.is_stopped():
+		waveInfo = floor(Timers.generation.time_left)
+	goblins.update_goblin_hud(current_population, waveInfo)
