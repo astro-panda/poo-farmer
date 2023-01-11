23281aecd31300e184d5a09dfc9e612b8aa64b0b
Caleb Haldane <caldane@gmail.com>
2023-01-10 23:43:44 -0600

    Add Barn Glow and Reimport images

--- poofarmer/Scripts/Silo.gd
+++ poofarmer/Scripts/Silo.gd
@@ -2,13 +2,16 @@ extends Area2D
 
 onready var player = get_tree().get_nodes_in_group("player")[0]
 onready var barn_sprite = $BarnSprite
+onready var glow = $Glow
 
 func _physics_process(delta):
 	if overlaps_body(player):
+		glow.visible = true
 		barn_sprite.animation = "open-doors"
 		print("player in dump range")
 		
 		if Input.is_action_pressed("ui_select"):
 			(player as Player).dump_poo_in_silo()
 	else:
+		glow.visible = false
 		barn_sprite.animation = "closed-doors"
