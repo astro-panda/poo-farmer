c23b02a74c2864bb56cd3942ca905bf28c39131d
Turner Bass <turner@astropanda.studio>
2023-01-10 22:49:40 -0600

    fixed poo drop off with space bar and animated it

--- poofarmer/Scripts/Silo.gd
+++ poofarmer/Scripts/Silo.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
 onready var player = get_tree().get_nodes_in_group("player")[0]
+onready var barn_sprite = $BarnSprite
 
 func _physics_process(delta):
 	if overlaps_body(player):
-		#$SiloSprite.animation = "open-doors"
+		barn_sprite.animation = "open-doors"
 		print("player in dump range")
 		
 		if Input.is_action_pressed("ui_select"):
 			(player as Player).dump_poo_in_silo()
+	else:
+		barn_sprite.animation = "closed-doors"
