df758181004482aabdc4752d4c9b028c441a5db4
Turner Bass <turner@astropanda.studio>
2023-01-10 23:13:32 -0600

    fixed gnome action to open store

--- poofarmer/Scripts/Gnome.gd
+++ poofarmer/Scripts/Gnome.gd
@@ -5,28 +5,16 @@ signal store_opened(opened)
 
 onready var sprite = $AnimatedSprite
 onready var glow = $AnimatedSprite/Glow
+onready var player = get_tree().get_nodes_in_group("player")[0]
 
 var player_in_range = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta):
+	player_in_range = overlaps_body(player)
 	
-func _unhandled_input(event):	
-	var should_open_store = event is InputEventMouseButton && (event as InputEventMouseButton).button_index == 1 && player_in_range && event.is_pressed()
-	if should_open_store:
+	if player_in_range && Input.is_action_pressed("ui_select"):
 		emit_signal("store_opened", true)
-	elif event.is_action_pressed("store"):
-		emit_signal("store_opened", false)
 
-func _on_Gnome_area_entered(body: Node):
-	if(body.is_in_group("player")):
-		player_in_range = true
-
-
-func _on_Gnome_area_exited(body: Node):	
-	if(body.is_in_group("player")):
-		player_in_range = false
 
 func playAnimation(direction):
 	sprite.animation = direction
