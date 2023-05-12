737a33d1408e56d324483ab0e6abaf1babb0abeb
Turner Bass <turner@astropanda.studio>
2023-05-11 22:54:47 -0500

    fix ammo UI handling and poo pellet typing

--- poofarmer/Scripts/PooPelletsManager.gd
+++ poofarmer/Scripts/PooPelletsManager.gd
@@ -2,45 +2,50 @@ extends Node2D
 
 onready var poo_pellets = preload("res://Scenes/PooPellets.tscn")
 
-func _on_firePoo(pelletInstance, spawnPosition, angleToMouse, mouseClick, fireMode):
-	if fireMode == FireMode.values.Shatgun:
-		var leftPellet = poo_pellets.instance()
-		leftPellet.damage = pelletInstance.damage
-		leftPellet.poo_speed = pelletInstance.poo_speed
-		leftPellet.distance = pelletInstance.distance
-		var rightPellet = poo_pellets.instance()
-		rightPellet.damage = pelletInstance.damage
-		rightPellet.poo_speed = pelletInstance.poo_speed
-		rightPellet.distance = pelletInstance.distance
-		
-		pelletInstance.global_position = spawnPosition
-		pelletInstance.startLocation = spawnPosition
-		leftPellet.global_position = spawnPosition
-		leftPellet.startLocation = spawnPosition
-		rightPellet.global_position = spawnPosition
-		rightPellet.startLocation = spawnPosition
-		
-		pelletInstance.set_direction(angleToMouse)
-		leftPellet.set_direction(angleToMouse.rotated(PI / 12))
-		rightPellet.set_direction(angleToMouse.rotated(- PI / 12))
-		add_child(pelletInstance)
-		add_child(leftPellet)
-		add_child(rightPellet)
-	elif fireMode == FireMode.values.RocketLauncher:
-		pelletInstance.global_position = spawnPosition
-		pelletInstance.startLocation = spawnPosition
-		pelletInstance.set_direction(angleToMouse)
-		pelletInstance.willExplode = true
-		add_child(pelletInstance)
-	elif fireMode == FireMode.values.Railgun:
-		pelletInstance.global_position = spawnPosition
-		pelletInstance.startLocation = spawnPosition
-		pelletInstance.set_direction(angleToMouse)
-		pelletInstance.isRailShot = true
-		pelletInstance.rotateAngle = spawnPosition.angle_to_point(mouseClick)
-		add_child(pelletInstance)
-	else:
-		pelletInstance.global_position = spawnPosition
-		pelletInstance.startLocation = spawnPosition
-		pelletInstance.set_direction(angleToMouse)
-		add_child(pelletInstance)
+func _on_firePoo(pellet, spawn_position, angle_to_target, target_position):
+	match pellet.fire_mode:
+		FireMode.values.Shatgun:
+			var leftPellet = poo_pellets.instance()
+			leftPellet.damage = pellet.damage
+			leftPellet.poo_speed = pellet.poo_speed
+			leftPellet.distance = pellet.distance
+			
+			var rightPellet = poo_pellets.instance()
+			rightPellet.damage = pellet.damage
+			rightPellet.poo_speed = pellet.poo_speed
+			rightPellet.distance = pellet.distance
+			
+			pellet.global_position = spawn_position
+			pellet.startLocation = spawn_position
+			leftPellet.global_position = spawn_position
+			leftPellet.startLocation = spawn_position
+			rightPellet.global_position = spawn_position
+			rightPellet.startLocation = spawn_position
+
+			pellet.set_direction(angle_to_target)
+			leftPellet.set_direction(angle_to_target.rotated(PI / 12))
+			rightPellet.set_direction(angle_to_target.rotated(- PI / 12))
+			add_child(pellet)
+			add_child(leftPellet)
+			add_child(rightPellet)
+
+		FireMode.values.RocketLauncher:
+			pellet.global_position = spawn_position
+			pellet.startLocation = spawn_position
+			pellet.set_direction(angle_to_target)
+			pellet.willExplode = true
+			add_child(pellet)
+
+		FireMode.values.Railgun:
+			pellet.global_position = spawn_position
+			pellet.startLocation = spawn_position
+			pellet.set_direction(angle_to_target)
+			pellet.isRailShot = true
+			pellet.rotateAngle = spawn_position.angle_to_point(target_position)
+			add_child(pellet)
+
+		_:
+			pellet.global_position = spawn_position
+			pellet.startLocation = spawn_position
+			pellet.set_direction(angle_to_target)
+			add_child(pellet)
--- poofarmer/Scripts/Pooters/Pooter.gd
+++ poofarmer/Scripts/Pooters/Pooter.gd
@@ -5,15 +5,16 @@ signal firePoo(pelletInstance, spawnPosition, angleToMouse, mouseClick, fireMode
 signal on_ammo_used(used_amount)
 
 export(PackedScene) var poo_pellets
-export(FireMode.values) var currentFireMode = FireMode.values.Shovel
+export(FireMode.values) var fire_mode = FireMode.values.Shovel
 export(Array, AudioStream) var poot_sounds
-export var enabled: bool = true
+export var enabled: bool = false
 export var cooldown: float = 0.25
 export var damage: int = 2
 export var pooSpeed: int = 7
 export var pooDistance: int = 300
 export var cost: float = 1
 export var ammo_max: float = 10
+export var current_ammo: float = 0
 
 onready var poot_player = $PootSound
 onready var cooldown_timer = $FireCooldown
@@ -25,20 +26,21 @@ func _ready():
 	connect("firePoo", PooPelletsManager, "_on_firePoo")
 	cooldown_timer.connect("timeout", self, "_on_FireCooldown_timeout")
 
-func shoot(target: Vector2, currentAmmo: float, inifite_ammo: bool):
-	if enabled && (currentAmmo >= cost || inifite_ammo):
+func shoot(target: Vector2, inifite_ammo: bool):
+	if enabled && (current_ammo >= cost || inifite_ammo):
 		if !inifite_ammo:
-			currentAmmo -= cost
+			current_ammo -= cost
 			emit_signal("on_ammo_used", cost)
 		
 		var poo_pellets_instance = poo_pellets.instance()
 		poo_pellets_instance.damage = damage
 		poo_pellets_instance.poo_speed = pooSpeed
 		poo_pellets_instance.distance = pooDistance
+		poo_pellets_instance.fire_mode = fire_mode
 		
-		var direction_to_mouse = global_position.direction_to(target).normalized()
+		var target_direction = global_position.direction_to(target).normalized()
 		print("fire pellet")
-		emit_signal("firePoo", poo_pellets_instance, global_position, direction_to_mouse, target, currentFireMode)
+		emit_signal("firePoo", poo_pellets_instance, global_position, target_direction, target)
 		enabled = false
 		$FireCooldown.start()
 		poot()
